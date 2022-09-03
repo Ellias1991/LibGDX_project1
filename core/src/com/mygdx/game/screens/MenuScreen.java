80e5071d824cc078efe7f73c260dabd7b27be41f
Илья <llya.tumanow2015@yandex.ru>
2022-09-03 23:12:25 +0300

    создали атлас

---
+++ core/src/com/mygdx/game/screens/MenuScreen.java
@@ -0,0 +1,48 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.mygdx.game.Main;
+
+public class MenuScreen implements Screen {
+Game game;
+
+    public MenuScreen(Main main) {
+        game=main;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
